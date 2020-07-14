c77a2c458372c88f73dfd2374433f71d8f8f51b8
atharva0511 <49303169+atharva0511@users.noreply.github.com>
2020-07-14 15:18:10 +0530

    HUDupdate1

---
+++ SkyRage/Assets/TopRightPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopRightPanel : MonoBehaviour {
+
+    Transform playerPlane;
+    public Image altitudeDisplay;
+    float maxAltitude = 1000;
+	// Use this for initialization
+	void Start () {
+        playerPlane = GetComponentInParent<Plane>().transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        altitudeDisplay.fillAmount = playerPlane.position.y / maxAltitude;
+	}
+}
