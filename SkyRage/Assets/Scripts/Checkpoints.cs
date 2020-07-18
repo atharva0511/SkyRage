cf12ef271bc8e2be613d87a454a32d81d01311b8
atharva0511 <49303169+atharva0511@users.noreply.github.com>
2020-07-18 17:17:01 +0530

    Skyboxes!

--- SkyRage/Assets/Scripts/Checkpoints.cs
+++ SkyRage/Assets/Scripts/Checkpoints.cs
@@ -11,6 +11,7 @@ public class Checkpoints : MonoBehaviour {
     public int xp = 10;
     public int finishedXP = 50;
     
+
 	// Use this for initialization
 	void Start () {
         NextCheckpoint(0);
@@ -23,14 +24,14 @@ public class Checkpoints : MonoBehaviour {
 
     public void NextCheckpoint(int index)
     {
-        if (index >= transform.childCount-1)
+        if (index > transform.childCount-1)
         {
             return;
         }
         transform.GetChild(index).GetComponent<BoxCollider>().enabled = true;
         if(arrowTemp!=null)Destroy(arrowTemp);
         if (markerTemp != null) Destroy(markerTemp);
-        arrowTemp = Instantiate(arrow, transform.GetChild(index).position, Quaternion.LookRotation(transform.GetChild(index + 1).position - transform.GetChild(index).position), transform.GetChild(index));
+        if(index<transform.childCount-1)arrowTemp = Instantiate(arrow, transform.GetChild(index).position, Quaternion.LookRotation(transform.GetChild(index + 1).position - transform.GetChild(index).position), transform.GetChild(index));
         markerTemp = Instantiate(targetMarker, transform.GetChild(index).position, Quaternion.LookRotation(Vector3.up), transform.GetChild(index));
         Renderer renderer = transform.GetChild(index).GetComponent<Renderer>();
         renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 1);
