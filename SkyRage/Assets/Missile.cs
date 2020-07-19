0cffa0dee711fd309dca413c54a40fdd80da39d9
soumik-devil <54310378+soumik-devil@users.noreply.github.com>
2020-07-19 18:06:54 +0530

    Models update

---
+++ SkyRage/Assets/Missile.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+	public ParticleSystem emission;
+	public Rigidbody rb;
+	public float missileTimer = 15;
+	public float speed = 150;
+	public float turnRate = 15;
+	bool launched = false;
+	Transform target;
+	public float accl= 25;
+	public Transform testTarget;
+
+
+
+	// Use this for initialization
+	public void Start () {
+
+		LaunchMissile(testTarget);
+
+
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (launched) 
+        {
+			this.transform.rotation = Quaternion.LookRotation(target.position-transform.position);
+			if (rb.velocity.sqrMagnitude < speed * speed)
+            {
+				rb.AddForce(this.transform.forward * rb.mass * accl);
+				
+			}
+        }
+		
+	}
+
+
+
+	public void LaunchMissile(Transform target)
+    {
+		launched = true;
+		this.target = target;
+		
+    }
+
+}
+
+
---
+++ SkyRage/Assets/Turret.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+	public float range = 200;
+	public float turnSpeed = 1;
+	public Transform target = null;
+	public Transform turret;
+
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+
+		if (target != null)
+		{
+			Quaternion rot = Quaternion.LookRotation(target.position - turret.position);
+			turret.rotation = Quaternion.Slerp(turret.rotation, rot, Time.deltaTime * turnSpeed);
+
+	    }
+	}
+}
